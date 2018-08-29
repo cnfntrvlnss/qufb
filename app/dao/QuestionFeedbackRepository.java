ac41bf9db08fe3aff30125e667c3f2b0d27354e4
shurui zheng(郑树锐) <zhengshr@inspur.com>
2018-08-29 21:04:31 +0800

    写好了QuestionFeedbackRepository

--- app/dao/QuestionFeedbackRepository.java
+++ app/dao/QuestionFeedbackRepository.java
@@ -13,4 +13,10 @@ public interface QuestionFeedbackRepository {
 
     public CompletionStage<List<QuestionFeedback>> findAll();
 
+    CompletionStage<QuestionFeedback> findById(Integer id);
+
+    public CompletionStage<Void> save(QuestionFeedback fb);
+    public CompletionStage<Void> update(QuestionFeedback fb);
+    public CompletionStage<Void> updateNotNull(QuestionFeedback fb);
+
 }
