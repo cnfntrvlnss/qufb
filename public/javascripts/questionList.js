ded819aaab210cc2a7aacc25546fc8f9f37a5eb7
Alice Li (李欣)-云数据中心集团 <li.xin01@inspur.com>
2018-08-31 17:57:24 +0800

    备份表格的代码

--- public/javascripts/questionList.js
+++ public/javascripts/questionList.js
@@ -10,18 +10,42 @@
                 alert("error!!! " + data);
             }
         });*/
+        var classCss;
         $.ajax({
             url: "/listQuestion",
             success: function(data){
-                console.log("questions:", data);
                 $.each(data, function(i, e){
-                     $('#questionTable').append('<tr><td>' + e.questionId + '</td><td>' + e.questionTitle+ '</td><td>' + e.bugHeader + '</td><td>' + e.questionDescription + '</td> <td>' + e.feedbackSuggestion+ '</td> <td>' + e.feedbackTime+'</td></tr>');
+                    if(i % 2==0){
+                        classCss="even";
+                    }else{
+                        classCss="odd";
+                    }
+                     $('#questionTable').append('<tr class='+classCss+'><td>' + '<a>'+e.questionId+'</a>'+ '</td><td>' + e.questionTitle+ '</td><td>' + e.bugHeader + '</td><td>' + e.questionDescription + '</td> <td>' + e.feedbackSuggestion+ '</td> <td>' + getMyDate(e.feedbackTime)+'</td></tr>');
                  });
             },
             error: function(data){
                 alert("error!!! " + data);
             }
         });
+         function getMyDate(str){
+                   var oDate = new Date(str),
+                    oYear = oDate.getFullYear(),
+                   oMonth = oDate.getMonth()+1,
+                    oDay = oDate.getDate(),
+                   oHour = oDate.getHours(),
+                    oMin = oDate.getMinutes(),
+                     oSen = oDate.getSeconds(),
+                     oTime = oYear +'-'+ getzf(oMonth) +'-'+ getzf(oDay) +' '+ getzf(oHour) +':'+ getzf(oMin) +':'+getzf(oSen);//最后拼接时间
+                     return oTime;
+                };
+
+        function getzf(num){
+             if(parseInt(num) < 10){
+                 num = '0'+num;
+             }
+            return num;
+         }
+
 
         		/*	$('#questionTable').datagrid({
         				url: "/listQuestion",
